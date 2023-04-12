9daa01d8a6fa0a7c0c53dc24d05242e3e8e368bc
cksdn <cksdngh@gmail.com>
2023-04-12 10:49:28 +0900

    ãmysql fix

--- backend/model/mysql.js
+++ backend/model/mysql.js
@@ -1,11 +1,36 @@
+// const mysql = require("mysql");
+//
+// const db = mysql.createConnection({
+//     host:"localhost",
+//     port:"3306",
+//     user:"root",
+//     password:"Cksdn1324132$",
+//     database:"ghd",
+// })
+//
+// db.connect((err) => {
+//     if (err) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
+//     console.log('connected as id ' + db.threadId);
+// });
+//
+// module.exports = db;
+// mysql://
+// user : bf2ce2930817a6:
+// password : becaabba@
+// host : us-cdbr-east-06.cleardb.net/
+// database : heroku_1c1d9fde0d345ed
+// ?reconnect=true
+
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host:"localhost",
-    port:"3306",
-    user:"root",
-    password:"Cksdn1324132$",
-    database:"ghd",
+    host:"us-cdbr-east-06.cleardb.net",
+    user:"bf2ce2930817a6",
+    password:"becaabba",
+    database:"heroku_1c1d9fde0d345ed",
 })
 
 db.connect((err) => {
@@ -15,5 +40,3 @@ db.connect((err) => {
     }
     console.log('connected as id ' + db.threadId);
 });
-
-module.exports = db;
