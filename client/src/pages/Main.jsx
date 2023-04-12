250dd7b4ebb491a9c3a0eb146d2e74f8d5ca0ef5
cksdn <cksdngh@gmail.com>
2023-04-12 14:31:16 +0900

    kakao map

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -31,6 +31,7 @@ import articleInfoSliderImg4 from "../image/archives/archive_8.jpg"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, {Autoplay, EffectFade, Navigation, Pagination} from 'swiper';
 import 'swiper/swiper-bundle.min.css';
+import { Map, MapMarker } from "react-kakao-maps";
 SwiperCore.use([Autoplay, Pagination]);
 
 
@@ -129,31 +130,6 @@ const Main = (props) => {
             duration: 1500,
         });
     })
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${"16179d6a690a671e5b85578fcb60e249"}&autoload=false`;
-        document.head.appendChild(script);
-
-        script.onload = () => {
-            window.kakao.maps.load(() => {
-                const container = document.getElementById('map');
-                const options = {
-                    center: new window.kakao.maps.LatLng(37.54191769903512, 127.04701503065148 ),
-                    level: 2
-                };
-                const map = new window.kakao.maps.Map(container, options);
-                const mapTypeControl = new window.kakao.maps.MapTypeControl();
-                map.addControl(mapTypeControl, window.kakao.maps.ControlPosition.TOPRIGHT);
-                const zoomControl = new window.kakao.maps.ZoomControl();
-                map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
-                const markerPosition  = new window.kakao.maps.LatLng(37.54191769903512, 127.04701503065148);
-                const marker = new window.kakao.maps.Marker({
-                    position: markerPosition
-                });
-                marker.setMap(map);
-            });
-        };
-    }, []);
     return (
         <main>
             <section>
@@ -488,7 +464,14 @@ const Main = (props) => {
             <section className={style.location}>
                 <div className={style.location_contain}>
                     <h2>LOCATION</h2>
-                    <div id="map" style={{ width: '100%', height: '500px' }} />
+                    <Map
+                        center={{ lat: 33.5563, lng: 126.79581 }}
+                        style={{ width: "100%", height: "360px" }}
+                    >
+                        <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+                            <div style={{color:"#000"}}>goodhood</div>
+                        </MapMarker>
+                    </Map>
                 </div>
             </section>
         </main>
