4211ae6d2d4dce759af6d9aecc6a0bdac6f62a03
cksdn <cksdngh@gmail.com>
2023-04-14 02:39:12 +0900

    fix

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,18 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 8000;
 
+const mainRouter = require("./router/mainRouter");
+const signUpRouter = require("./router/signUpRouter");
+const loginRouter = require("./router/loginRouter");
+const noticeRouter = require("./router/noticeRouter");
+const inquiryRouter = require("./router/inquiryRouter");
+
 app.use((req, res, next) => {
     res.setHeader('Cache-Control', 'public, max-age=86400');
     next();
+});
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname +"/build/index.html")
 });
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -18,12 +27,6 @@ app.use(express.static("build"))
 app.get("/", function (req, res){
     res.sendFile(__dirname +"/build/index.html")
 })
-
-const mainRouter = require("./router/mainRouter");
-const signUpRouter = require("./router/signUpRouter");
-const loginRouter = require("./router/loginRouter");
-const noticeRouter = require("./router/noticeRouter");
-const inquiryRouter = require("./router/inquiryRouter");
 
 app.use("/api/main",mainRouter);
 app.use("/api/signUp",signUpRouter);
